cb113c7b5b14324e767a6ac72e5a4474b169687e
Md. Fazley Rabbi <fazley111@gmail.com>
2021-09-16 18:08:42 +0600

    sidebar autohide on page reload mobile view

--- includes/nav.js
+++ includes/nav.js
@@ -83,10 +83,25 @@ const sidebar = document.getElementById('sidebar');
 const mainwrapper = document.getElementById('main-wrapper');
 const footer = document.querySelector('footer');
 navbtn.addEventListener('click', () => {
+    trigger()
+})
+
+window.onload = () => {
+    if(screen.width < 800 || window.innerWidth < 800){
+        trigger()
+    }
+}
+
+
+function trigger() {
     sidebar.classList.toggle('active');
     mainwrapper.classList.toggle('active');
     footer.classList.toggle('active');
-})
+}
+
+
+
+
 
 
 // other navs 
